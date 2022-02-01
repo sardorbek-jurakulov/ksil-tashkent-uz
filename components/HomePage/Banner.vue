8e5518ba1c19eebac97862d53ea0dc1a90a8659a
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2022-02-01 12:00:01 +0500

    WIP

--- components/HomePage/Banner.vue
+++ components/HomePage/Banner.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="header">
-    <h2>ДЕТСКИЕ ПЛОЩАДКИ</h2>
+  <div class="header banner">
+    <h2 class="banner__heading">ДЕТСКИЕ ПЛОЩАДКИ</h2>
     <img src="@/assets/img/logo-first.png" width="794" height="518" alt="ДЕТСКИЕ ПЛОЩАДКИ">
     <a href="WWW.KSIL-TASHKENT.UZ" target="_self">WWW.KSIL-TASHKENT.UZ</a>
     <a href="tel:+998995002202">+998 99 500 22 02</a><a href="tel:+998995003303">+998 99 500 33 03</a>
