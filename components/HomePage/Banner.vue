82848662442703381ee21510a02a906c3fc3a350
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2022-02-01 15:11:33 +0500

    WIp

--- components/HomePage/Banner.vue
+++ components/HomePage/Banner.vue
@@ -2,13 +2,14 @@
   <div class="banner">
     <div class="banner__main-info-wrapper">
       <h2 class="banner__heading">ДЕТСКИЕ ПЛОЩАДКИ</h2>
-      <img class="banner__logo-image" src="@/assets/img/logo-first.png" width="790" height="510" alt="ДЕТСКИЕ ПЛОЩАДКИ">
+      <img class="banner__logo-image" src="@/assets/img/logo-first.png" width="620" height="410" alt="ДЕТСКИЕ ПЛОЩАДКИ">
       <a class="banner__site-link" href="WWW.KSIL-TASHKENT.UZ" target="_self">WWW.KSIL-TASHKENT.UZ</a>
       <div class="banner__contact-number-link-wrapper">
         <a class="banner__contact-number-link" href="tel:+998995002202">+998 99 500 22 02</a>
         <a class="banner__contact-number-link" href="tel:+998995003303">+998 99 500 33 03</a>
+      </div>
     </div>
-    </div>
+    <a class="banner__more-info-button" href="#" target="_self"> &#10095; </a>
   </div>
 </template>
 
