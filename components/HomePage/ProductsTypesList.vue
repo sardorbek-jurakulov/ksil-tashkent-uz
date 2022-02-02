be450587b62535e0f674a26847d6ec316ac96819
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2022-02-02 19:30:30 +0500

    WIP

--- components/HomePage/ProductsTypesList.vue
+++ components/HomePage/ProductsTypesList.vue
@@ -1,10 +1,30 @@
 <template>
   <div class="container p-5">
     <h2 class="text-primary text-center mb-5">ОБОРУДОВАНИЕ ДЛЯ ИГРОВЫХ ПЛОЩАДОК</h2>
-    <div>
+    <div class="">
       <a href="#" target="_self">
         <h3>Горки</h3>
-        <img src="" alt="">
+        <img src="@/assets/img/products-types/slides.jpg" alt="Горки" width="500" height="300">
+      </a>
+      <a href="#" target="_self">
+        <h3>Качели</h3>
+        <img src="@/assets/img/products-types/swing.jpg" alt="Качели" width="500" height="300">
+      </a>
+      <a href="#" target="_self">
+        <h3>Качалки на пружине</h3>
+        <img src="@/assets/img/products-types/rocking-chairs-on-a-spring.jpg" alt="Качалки на пружине" width="500" height="300">
+      </a>
+      <a href="#" target="_self">
+        <h3>Детские комплексы</h3>
+        <img src="@/assets/img/products-types/childrens-complexes.jpg" alt="Детские комплексы" width="500" height="300">
+      </a>
+      <a href="#" target="_self">
+        <h3>Песочницы</h3>
+        <img src="@/assets/img/products-types/sandboxes-2.jpg" alt="Песочницы" width="500" height="300">
+      </a>
+      <a href="#" target="_self">
+        <h3>Машинки и паровозики</h3>
+        <img src="@/assets/img/products-types/cars-and-locomotives.png" alt="Машинки и паровозики" width="500" height="300">
       </a>
     </div>
   </div>
