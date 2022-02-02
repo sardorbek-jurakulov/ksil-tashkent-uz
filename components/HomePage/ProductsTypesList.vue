da785486044737b1469738e9d705b262a4d03384
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2022-02-02 10:27:56 +0500

    WIP

--- components/HomePage/ProductsTypesList.vue
+++ components/HomePage/ProductsTypesList.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    <h2>Products Types List</h2>
+    <h2>ОБОРУДОВАНИЕ ДЛЯ ИГРОВЫХ ПЛОЩАДОК</h2>
+    <div>
+      <a href="#" target="_self">
+        <h3>Горки</h3>
+        <img src="" alt="">
+      </a>
+    </div>
   </div>
 </template>
 
