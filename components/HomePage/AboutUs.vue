1284a14952b3ea35231a4e59cdb2b0d2e1e8eebf
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2022-02-04 00:48:50 +0500

    WIP

--- components/HomePage/AboutUs.vue
+++ components/HomePage/AboutUs.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container d-flex flex-column align-items-center justify-content-center mb-4 mb-md-5 about-us-section about-us-section__container">
+  <div class="container d-flex flex-column align-items-center justify-content-center mb-5 about-us-section about-us-section__container">
     <h2 class="mb-3 mb-md-4 about-us-section__title">О НАС</h2>
     <div class="d-flex align-items-center justify-content-center flex-column flex-lg-row about-us-section__content-wrapper">
       <p class="w-75 d-flex align-items-center justify-content-center mb-3 mb-lg-0 p-0 p-lg-4 text-justify about-us-section__description">
--- components/SiteFooter.vue
+++ components/SiteFooter.vue
@@ -1,7 +1,39 @@
 <template>
-  <div>
-    <!--<h2>Site Footer</h2>-->
+  <div class="container-fluint site-footer site-footer__container">
+  <h2 class="site-footer__title">Узнайте о нас больше</h2>
+    <div>
+      <div>
+        <div>
+          <h3>Рабочие дни</h3>
+          <p>Понедельник - Суббота: 8:00 - 20:00</p>
+          <p>Воскресенье - 10:00 - 15:00</p>
+          <p>Звонки ежедневно до 22:00</p>
+        </div>
+        <div>
+          <h3>Наши контакты</h3>
+          <p>Телефоны и telegram: 
+            <a href="tel:+998995002202">+998 99 500 22 02</a>
+            <a href="tel:+998995003303">+998 99 500 33 03</a>
+          </p>
+          <p>Электронная почта:</p>
+          <a href="mailto:info@ksil-tashkent.uz">info@ksil-tashkent.uz</a>
+        </div>
+      </div>
+      <div>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5992.781662252687!2d69.283118!3d41.322114!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae8b3411210e17%3A0x2665e21a0c15fbfe!2s60%20Amir%20Temur%20Avenue%2C%20Tashkent%2C%20Uzbekistan!5e0!3m2!1sen!2sus!4v1643916282913!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+      </div>
+    </div>
   </div>
 </template>
 
 <script></script>
+
+<style>
+.site-footer__container {
+  background-color: #ccc;
+  background-image: url("@/assets/img/footer/footer-bg.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+}
+</style>
