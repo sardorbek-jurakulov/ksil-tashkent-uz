e3f930a28ac7ed6d51a2ba15cf6939f37060b80b
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2022-02-03 11:48:26 +0500

    WIP

--- components/HomePage/ProductsTypesList.vue
+++ components/HomePage/ProductsTypesList.vue
@@ -6,11 +6,11 @@
       flex-wrap 
       flex-md-row 
       flex-column
+      justify-content-center
       justify-content-xl-between
       justify-content-lg-around
-      justify-content-md-between
       align-items-center 
-      products-types-container-items-wrapper 
+      products-types-container__items-wrapper 
       border border-primary
     ">
       <a class="
@@ -19,86 +19,121 @@
       justify-content-center 
       align-items-center 
       mb-3 
-      mb-md-4 
-      mb-lg-5 
-      products-types-container-item
-      " href="#" target="_self">
-        <h3 class="order-1 mb-4">Горки</h3>
-        <img class="mb-4" src="@/assets/img/products-types/slides.jpg" alt="Горки" width="300" height="200">
-      </a>
-      <a class="
-      d-flex 
-      flex-column 
-      justify-content-center 
-      align-items-center 
-      mb-3 
-      mb-md-4 
-      mb-lg-5 
-      products-types-container-item
-      " href="#" target="_self">
-        <h3 class="order-1 mb-4">Качели</h3>
-        <img class="mb-4" src="@/assets/img/products-types/swing.jpg" alt="Качели" width="300" height="200">
-      </a>
-      <a class="
-      d-flex 
-      flex-column 
-      justify-content-center 
-      align-items-center 
-      mb-3 
-      mb-md-4 
-      mb-lg-5 
-      products-types-container-item
-      " href="#" target="_self">
-        <h3 class="order-1 mb-4">Качалки на пружине</h3>
-        <img class="mb-4" src="@/assets/img/products-types/rocking-chairs-on-a-spring.jpg" alt="Качалки на пружине" width="300" height="200">
-      </a>
-      <a class="
-      d-flex 
-      flex-column 
-      justify-content-center 
-      align-items-center 
-      mb-3 
-      mb-md-4 
-      mb-lg-5 
-      products-types-container-item
+      mb-md-4
+      products-types-container__item
       " href="#" target="_self">
         <h3 class="
         order-1 
-        mb-4
+        mb-3
+        mb-md-4
+        px-2
+        ">Горки</h3>
+        <img class="
+        mb-3
+        mb-md-4
+        products-types-container__item-image
+        " src="@/assets/img/products-types/slides.jpg" alt="Горки" width="300" height="200">
+      </a>
+      <a class="
+      d-flex 
+      flex-column 
+      justify-content-center 
+      align-items-center 
+      mb-3
+      mb-md-4
+      products-types-container__item
+      " href="#" target="_self">
+        <h3 class="
+        order-1 
+        mb-3
+        mb-md-4
+        px-2
+        ">Качели</h3>
+        <img class="
+        mb-3
+        mb-md-4
+        " src="@/assets/img/products-types/swing.jpg" alt="Качели" width="300" height="200">
+      </a>
+      <a class="
+      d-flex 
+      flex-column 
+      justify-content-center 
+      align-items-center 
+      mb-3
+      mb-md-4
+      products-types-container__item
+      " href="#" target="_self">
+        <h3 class="
+        order-1 
+        mb-3
+        mb-md-4
+        px-2
+        ">Качалки на пружине</h3>
+        <img class="
+        mb-3
+        mb-md-4
+        " src="@/assets/img/products-types/rocking-chairs-on-a-spring.jpg" alt="Качалки на пружине" width="300" height="200">
+      </a>
+      <a class="
+      d-flex 
+      flex-column 
+      justify-content-center 
+      align-items-center 
+      mb-3
+      mb-md-4
+      products-types-container__item
+      " href="#" target="_self">
+        <h3 class="
+        order-1 
+        mb-3
+        mb-md-4
+        px-2
         ">Детские комплексы</h3>
-        <img class="mb-4" src="@/assets/img/products-types/childrens-complexes.jpg" alt="Детские комплексы" width="300" height="200">
+        <img class="
+        mb-3
+        mb-md-4
+        " src="@/assets/img/products-types/childrens-complexes.jpg" alt="Детские комплексы" width="300" height="200">
       </a>
       <a class="
       d-flex 
       flex-column 
       justify-content-center 
       align-items-center 
-      mb-3 
-      mb-md-4 
-      mb-lg-5 
-      products-types-container-item
+      mb-3
+      mb-md-4
+      products-types-container__item
       " href="#" target="_self">
         <h3 class="
         order-1 
-        mb-4
+        mb-3
+        mb-md-4
+        px-2
         ">Песочницы</h3>
-        <img class="mb-4" src="@/assets/img/products-types/sandboxes-2.jpg" alt="Песочницы" width="300" height="200">
+        <img class="
+        mb-3
+        mb-md-4
+        " src="@/assets/img/products-types/sandboxes-2.jpg" alt="Песочницы" width="300" height="200">
       </a>
       <a class="
       d-flex 
       flex-column 
       justify-content-center 
       align-items-center 
-      mb-3 
-      mb-md-4 
-      mb-lg-5 
-      products-types-container-item
+      mb-3
+      mb-md-4
+      products-types-container__item
       " href="#" target="_self">
         <h3 class="
         order-1 
-        mb-4
+        mb-3
+        mb-md-4
+        px-2
         ">Машинки и паровозики</h3>
-        <img class="mb-4" src="@/assets/img/products-types/cars-and-locomotives.png" alt="Машинки и паровозики" width="300" height="200">
+        <img class="
+        mb-3
+        mb-md-4
+        products-types-container__item-image
+        " src="@/assets/img/products-types/cars-and-locomotives.png" alt="Машинки и паровозики" width="300" height="200">
       </a>
     </div>
   </div>
@@ -107,21 +142,25 @@
 <script></script>
 
 <style scoped>
-.products-types-container-item {
+.products-types-container__item {
   color: #007bff;
   border: 2px solid #ffffff;
   border-radius: 5px;
   transition: box-shadow ease 0.3s;
 }
 
-.products-types-container-item:hover {
+.products-types-container__item:hover {
   color: #0063cc;
   border: 2px solid #0063cc;
   text-decoration: none;
   box-shadow: -8px 8px 5px #ccc;
 }
 
-.products-types-container-item:active {
+.products-types-container__item:active {
   box-shadow: 0px 0px 0px #ccc;
+}
+
+.products-types-container__item-image {
+  min-width: 100%;
 }
 </style>
