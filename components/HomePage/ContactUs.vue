1e0cca8235e9270f2b4222eb7deace6d520c7894
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2022-02-03 18:16:15 +0500

    WIP

--- components/HomePage/ContactUs.vue
+++ components/HomePage/ContactUs.vue
@@ -1,6 +1,25 @@
 <template>
-  <div class="container">
-    <!-- <h2>Contact Us</h2> -->
+  <div class="
+  container-fluid
+  contact-us contact-us__container
+  ">
+    <h2 class="
+    text-center 
+    text-primary 
+    mb-5
+    contact-us__title
+    ">СВЯЖИТЕСЬ С НАМИ</h2>
+    <ul class="
+    contact-us__features-list
+    ">
+      <li>- Гибкие цены</li>
+      <li>- Гарантийный период</li>
+      <li>- Монтажное сопровождение</li>
+      <li>- Регулярный осмотр объектов</li>
+      <li>- Паспорта и сертификаты</li>
+      <li>- Отгрузка по Узбекистану-1 день</li>
+    </ul>
+    <button class="cont">связаться с нами</button>
   </div>
 </template>
 
