6634a1f677849f0654d29ed1421e72fd22d682c0
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2022-02-03 10:21:17 +0500

    WIP

--- components/HomePage/ProductsTypesList.vue
+++ components/HomePage/ProductsTypesList.vue
@@ -6,24 +6,24 @@
         <h3 class="order-1 mb-4">Горки</h3>
         <img class="mb-4" src="@/assets/img/products-types/slides.jpg" alt="Горки" width="300" height="200">
       </a>
-      <a class="d-flex flex-column justify-content-center align-items-center mb-5" href="#" target="_self">
-        <h3 class="order-1">Качели</h3>
+      <a class="d-flex flex-column justify-content-center align-items-center mb-5 products-types-container-item" href="#" target="_self">
+        <h3 class="order-1 mb-4">Качели</h3>
         <img class="mb-4" src="@/assets/img/products-types/swing.jpg" alt="Качели" width="300" height="200">
       </a>
-      <a class="d-flex flex-column justify-content-center align-items-center mb-5" href="#" target="_self">
-        <h3 class="order-1">Качалки на пружине</h3>
+      <a class="d-flex flex-column justify-content-center align-items-center mb-5 products-types-container-item" href="#" target="_self">
+        <h3 class="order-1 mb-4">Качалки на пружине</h3>
         <img class="mb-4" src="@/assets/img/products-types/rocking-chairs-on-a-spring.jpg" alt="Качалки на пружине" width="300" height="200">
       </a>
-      <a class="d-flex flex-column justify-content-center align-items-center mb-5" href="#" target="_self">
-        <h3 class="order-1">Детские комплексы</h3>
+      <a class="d-flex flex-column justify-content-center align-items-center mb-5 products-types-container-item" href="#" target="_self">
+        <h3 class="order-1 mb-4">Детские комплексы</h3>
         <img class="mb-4" src="@/assets/img/products-types/childrens-complexes.jpg" alt="Детские комплексы" width="300" height="200">
       </a>
-      <a class="d-flex flex-column justify-content-center align-items-center mb-5" href="#" target="_self">
-        <h3 class="order-1">Песочницы</h3>
+      <a class="d-flex flex-column justify-content-center align-items-center mb-5 products-types-container-item" href="#" target="_self">
+        <h3 class="order-1 mb-4">Песочницы</h3>
         <img class="mb-4" src="@/assets/img/products-types/sandboxes-2.jpg" alt="Песочницы" width="300" height="200">
       </a>
-      <a class="d-flex flex-column justify-content-center align-items-center mb-5" href="#" target="_self">
-        <h3 class="order-1">Машинки и паровозики</h3>
+      <a class="d-flex flex-column justify-content-center align-items-center mb-5 products-types-container-item" href="#" target="_self">
+        <h3 class="order-1 mb-4">Машинки и паровозики</h3>
         <img class="mb-4" src="@/assets/img/products-types/cars-and-locomotives.png" alt="Машинки и паровозики" width="300" height="200">
       </a>
     </div>
