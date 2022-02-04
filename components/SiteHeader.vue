22e99d7f28c7b4c62b5d42a6e98cb74a2ed3852b
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2022-02-04 23:03:33 +0500

    WIP

--- components/SiteHeader.vue
+++ components/SiteHeader.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
-    <a href=""></a>
+    <nav class="navbar navbar-expand-sm bg-white navbar-white fixed-top">
+      <a class="navbar-brand" href="#">Logo</a>
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+      </ul>
+    </nav>
   </div>
 </template>
 
