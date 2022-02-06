6a9599a6d91c26a9340e44e1bce82b1ef944e6bf
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2022-02-07 00:33:29 +0500

    WIP

--- components/Products/ProductCard.vue
+++ components/Products/ProductCard.vue
@@ -17,26 +17,63 @@
       px-2
       ">{{ cardTitle }}</h3>
       <div 
-      :style="{backgroundImage: cardImgPath}"  
+      :style="{ 'background-image': `url(${cardImgPath1})` }"  
       class="
         mb-3
         mb-md-4
+        card-img-div
       "></div>
     </nuxt-link>
   </div>
 </template>
+
 <script>
+// import productsImgesPathes from "~/assets/img/products";
 export default {
+  data() {
+    return {
+      // productsImgesPathes,
+      cardImgPath1: require('~/assets/img/products/slides/004202/1.png'),
+    }
+  },
   props: {
     cardTitle: {
       type: String,
       required: true,
     },
+    // cardImagesCategory: {
+    //   type: String,
+    //   required: true,
+    // },
+    // cardProductCompanyId: {
+    //   type: Number,
+    //   required: true,
+    // },
     cardImgPath: {
       type: String,
       required: true,
     }
+  },
+  computed: {
+    // cardProductImgPath() {
+    //   return this.productsImgesPathes.find(categories => {
+    //     category
+    //   })
+    // }
+  }, 
+  mounted() {
+    return console.log(this.cardTitle);
   }
 }
 </script>
-<style></style>
+
+<style scoped>
+.card-img-div {
+  width: 300px;
+  height: 300px;
+  background-color: #ccc;
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
